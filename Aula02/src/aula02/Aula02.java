7ab0b7a2dbd2baf9c2a1c4a60f935fc356bb0366
Lucas Fernandes Paixão Dos Santos <lucasfpd.santos@gmail.com>
2021-07-06 18:13:28 -0300

    Aula 03

--- Aula02/src/aula02/Aula02.java
+++ Aula02/src/aula02/Aula02.java
@@ -7,19 +7,13 @@ public class Aula02 {
     public static void main(String[] args) {
         System.out.println("Caneta c1");
         Caneta c1 = new Caneta();
-        c1.cor = "azul";
-        c1.ponta = 0.5f;
-        c1.tampar();
-        c1.rabiscar();
+        c1.modelo = "BIC cristal";
+        c1.cor = "Azul";
+        //c1.ponta = 0.5f;
+        c1.carga = 80;
+        c1.tampada = true;
         c1.status();
         
-        System.out.println("Caneta c2");
-        Caneta c2 = new Caneta();
-        c2.modelo = "Hostnet";
-        c2.cor = "Preta";
-        c2.destampar();
-        c2.rabiscar();
-        c2.status();
     }
     
 }
--- Aula02/src/aula02/Caneta.java
+++ Aula02/src/aula02/Caneta.java
@@ -3,20 +3,20 @@ package aula02;
 
 
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    protected boolean tampada;
     
-    void status(){
+    public void status(){
         System.out.println("Modelo: " + this.modelo);
         System.out.println("Uma caneta " + this.cor);
         System.out.println("Ponta: " + this.ponta);
         System.out.println("Carga: " + this.carga);
         System.out.println("Está tampada? " + this.tampada);
     }
-    void rabiscar() {
+    public void rabiscar() {
         if (this.tampada == true){
             System.out.println("ERRO! Não posso rabiscar");
         } else {
@@ -24,11 +24,11 @@ public class Caneta {
         }
     }
     
-    void tampar() {
+    protected void tampar() {
         this.tampada = true;
     }
     
-    void destampar() {
+    protected void destampar() {
         this.tampada = false;
     }
     
